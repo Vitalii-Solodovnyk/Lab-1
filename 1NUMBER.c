fb8c98fdbe1b5c4b51839c44fbca53f676bb4403
Vitalii-Solodovnyk <vitalii.solodovnyk@student.ukw.edu.pl>
2025-10-19 21:30:07 +0200

    Add files via upload

---
+++ 1NUMBER.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+    float amountPLN;
+    float rate;
+    float amountUSD;
+
+    printf("KONWERTER WALUT PLN -> USD\n");
+    printf("Podaj kwotę w PLN: ");
+    scanf("%f", &amountPLN);
+
+    printf("Podaj kurs USD/PLN (np. 4.25): ");
+    scanf("%f", &rate);
+
+    if (rate <= 0) {
+        printf("Błąd: kurs musi być większy od zera!\n");
+        return 1;
+    }
+
+    amountUSD = amountPLN / rate;
+
+    printf("\n%.2f PLN = %.2f USD\n", amountPLN, amountUSD);
+
+    return 0;
+}
---
+++ Number2.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+	float purchaseNet;
+	float marginPercent;
+	float sellingPrice;
+
+
+	printf("Program: Marza (cena sprzedazy)\n");
+
+	printf("Podaj cene zakupu netto (np. 100.00): ");
+	scanf("%f", &purchaseNet);
+	
+	printf("Podaj marze w procentach (np. 25): ");
+	scanf("%f", &marginPercent);
+
+	sellingPrice = purchaseNet * (1.0f + marginPercent / 100.0f);
+
+	printf("\nCena sprzedazy = %f\n", sellingPrice);
+
+	return 0;
+}
